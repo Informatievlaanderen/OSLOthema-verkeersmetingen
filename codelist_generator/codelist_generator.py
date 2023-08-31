0fe9b70d715213bcbc5da735e79b2998b849c578
Samuel Van Ackere <samuel.vanackere@vlaanderen.be>
2023-08-31 11:15:13 +0200

    update

--- codelist_generator/codelist_generator.py
+++ codelist_generator/codelist_generator.py
@@ -9,7 +9,7 @@ translator = Translator()
 
 dataframe = load_workbook('codelists.xlsx')
 
-languages = ["en", "fr", "de"]
+languages = ["en"]
 
 # Define filepath
 filepath = 'codelists.xlsx'
@@ -36,10 +36,13 @@ for sheet in f.sheet_names:
 
 # Combine all DataFrames into one
 data = pd.concat(list_of_dfs, ignore_index=True)
-
+tel_nan = 0
 
 
 text = "@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> . \n"
+# > .
+text = text + "@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema> . \n"
+
 text = text + "@prefix skos: <http://www.w3.org/2004/02/skos/core#> . \n"
 
 text = text + "\n"
@@ -47,56 +50,72 @@ text = text + "\n"
 #for i in range(len(list_of_sheets)):
 #    print(list_of_sheets[i])
 
-sheet = 1
+sheet = 0
 
 text = text + "<https://data.vlaanderen.be/id/conceptscheme/" + \
-    str(list_of_sheets[sheet].replace(" ", "_")) + ">  a skos:ConceptScheme ; \n"
-text = text + "<https://www.w3.org/ns/adms#status> <https://wegenenverkeer.data.vlaanderen.be/id/concept/VkmStatus/ingebruik > ; \n"
-text = text + "\n"
+    str(list_of_dfs[sheet]["Klasse"][0]).replace(" ", "_").lower() + ">  a skos:ConceptScheme ; \n"
+text = text + "<https://www.w3.org/ns/adms#status> <https://wegenenverkeer.data.vlaanderen.be/id/concept/VkmStatus/ingebruik> ; \n"
 
 text = text + 'skos:prefLabel "' + str(list_of_dfs[sheet]["Label"][0]) + '"@nl ; \n'
 text = text + 'skos:definition "' + \
     str(list_of_dfs[sheet]["Definitie"][0]) + '"@nl ; \n'
+text = text + '\n'
 
 
 
-for i in range(len(list_of_dfs)-1):
-    i = i+1
-    text = text + "<https://data.vlaanderen.be/id/conceptscheme/" + \
-        str(list_of_dfs[sheet]["Klasse"][i]) + ">  a skos:Concept ; \n"
-    text = text + "<https://www.w3.org/ns/adms#status> <https://wegenenverkeer.data.vlaanderen.be/id/concept/VkmStatus/" + str(list_of_dfs[sheet]["Status"][i]) +" > ; \n"
+for i in range(len(list_of_dfs[sheet])):
+    if (i ==0):
+        continue
+    print(str(list_of_dfs[sheet]["Klasse"][i]))
     
-    text = text + 'skos:definition "' + \
-        str(list_of_dfs[sheet]["Definitie"][i]) + '"@nl ; \n'
+    if (str(list_of_dfs[sheet]["Klasse"][i]) == "Nan"):
+        tel_nan= tel_nan + 1
+        continue
+    
+    
+    else:
+        text = text + "<https://data.vlaanderen.be/id/conceptscheme/" + str(list_of_dfs[sheet]["Klasse"][0]).replace(" ", "_").lower() + "/" + \
+            str(list_of_dfs[sheet]["Klasse"][i]).replace(' ', '_').lower() + ">  a skos:Concept ; \n"
+        text = text + "<https://www.w3.org/ns/adms#status> <https://wegenenverkeer.data.vlaanderen.be/id/concept/VkmStatus/" + str(list_of_dfs[sheet]["Status"][i]) + "> ; \n"
         
-    text = text + 'skos:inscheme "' + \
-        str(list_of_dfs[sheet]["Definitie"][i]) + '"@nl ; \n'
-
-    
-    for language in languages:
         text = text + 'skos:definition "' + \
-            translator.translate(
-                str(list_of_dfs[sheet]["Definitie"][i]), dest=language).text + '"@' + language + ' ; \n'
+            str(list_of_dfs[sheet]["Definitie"][i]) + '"@nl ; \n'
+            
+        text = text + 'skos:inscheme ' + \
+            "<https://data.vlaanderen.be/id/conceptscheme/" + \
+            str(list_of_sheets[sheet].replace(" ", "_")) + '> ; \n'
+
+        text = text + 'skos:notation "' + \
+            str(list_of_dfs[sheet]["Notation"][i]) + '" ; \n'
         text = text + 'skos:prefLabel "' + \
-            translator.translate(
-                str(list_of_dfs[sheet]["Label"][i]), dest=language).text + '"@' + language + ' ; \n'
+            str(list_of_dfs[sheet]["Label"][i]) + '"@nl ; \n'
+            
         
+        for language in languages:
+            text = text + 'skos:definition "' + \
+                translator.translate(
+                    str(list_of_dfs[sheet]["Definitie"][i]), dest=language).text + '"@' + language + ' ; \n'
+            text = text + 'skos:prefLabel "' + \
+                translator.translate(
+                    str(list_of_dfs[sheet]["Label"][i]), dest=language).text + '"@' + language + ' ; \n'
+                
+        if {'subklassevan'}.issubset(list_of_dfs[sheet].columns):
+            text = text + 'rdfs:subClassOf  <https://data.vlaanderen.be/id/concept/' + \
+                str(list_of_dfs[sheet]["subklassevan"][i]) + '> ; \n'
 
-    text = text + 'skos:inscheme <https://data.vlaanderen.be/id/conceptscheme/' + \
-        str(list_of_sheets[sheet].replace(" ", "_")) + '>'
-    text = text + 'skos:notation "' + str(list_of_dfs[sheet]["Notation"][i]) + '" ; \n'
-    text = text + 'skos:prefLabel "' + \
-        str(list_of_dfs[sheet]["Label"][i]) + '"@nl ; \n'
+
+
+        
+        text = text + 'skos:topConceptOf <https://data.vlaanderen.be/id/conceptscheme/' + \
+            str(list_of_sheets[sheet].replace(" ", "_")) + '> . \n'
+
+        text = text + "\n"
     
-    text = text + 'skos:topConceptOf <https://data.vlaanderen.be/id/conceptscheme/' + \
-        str(list_of_sheets[sheet].replace(" ", "_")) + '> . \n'
-
-    text = text + "\n"
-
-print(text)
 
 
-with open('VkmMeettechnieken.ttl', 'w') as f:
+
+
+with open(str(list_of_dfs[sheet]["Klasse"][0]) + '.ttl', 'w') as f:
     f.write(text)
     
-
+print(str(tel_nan) + ' members niet kunnen parsen naar codelijst')
